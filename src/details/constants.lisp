85b763df74ee57beaac0f1e400462550dd4bc5e8
Alexey Veretennikov <txm.fourier@gmail.com>
2017-12-29 12:51:05 +0100

    Implemented normpath for posix

---
+++ src/details/constants.lisp
@@ -0,0 +1,21 @@
+(defpackage py.path.details.constants
+  (:use :cl :alexandria)
+  (:export
+   +separator+
+   +posix-separator+
+   +unc-prefix+
+   +path-separator+
+   +current-dir+
+   +up-dir+
+   +secs-between-epochs+))
+
+(in-package py.path.details.constants)
+
+(define-constant +separator+  #+windows #\\ #-windows #\/)
+(define-constant +posix-separator+ #\/)
+(define-constant +unc-prefix+ "//" :test #'equal)
+(define-constant +path-separator+ #+windows #\; #-windows #\:)
+(define-constant +current-dir+ "." :test #'equal)
+(define-constant +up-dir+ ".." :test #'equal)
+(define-constant +secs-between-epochs+ 11644473600
+                 :documentation "Seconds between 1.1.1601 and 1.1.1970")
